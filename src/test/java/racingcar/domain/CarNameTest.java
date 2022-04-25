92bfe1c7def707735d3720ecf78d89866b1f8000
toughchb <ggang2000@gmail.com>
2022-04-25 12:57:01 +0900

    refactor(CarNameTest): 자동차 이름 검증 테스트 메세지 확인 추가

--- src/test/java/racingcar/domain/CarNameTest.java
+++ src/test/java/racingcar/domain/CarNameTest.java
@@ -2,7 +2,6 @@ package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CarNameTest {
@@ -11,15 +10,18 @@ public class CarNameTest {
     void 자동차_이름을_검증한다() {
         assertThatThrownBy(
                 () -> new CarName("")
-        ).isInstanceOf(IllegalArgumentException.class);
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차의 이름이 존재 해야 합니다.");
 
         assertThatThrownBy(
                 () -> new CarName(null)
-        ).isInstanceOf(IllegalArgumentException.class);
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차의 이름이 존재 해야 합니다.");
 
         assertThatThrownBy(
                 () -> new CarName("abcdef")
-        ).isInstanceOf(IllegalArgumentException.class);
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 5 글자를 넘어갈 수 없습니다.");
     }
 
     @Test
